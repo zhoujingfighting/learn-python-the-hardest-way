863f82b26ca6d838a9256b725cf1f06a815f1f9c
zhoujingfighting <254644528@qq.com>
2024-01-15 04:58:14 +0800

    Update read

---
+++ read.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, filename = argv
+
+# txt = open(filename)
+
+# print(f"Here's your file {filename}")
+# try:
+#   print(txt.read())
+#   file_again = input("> ")
+#   txt_again = open(file_again)
+#   print(txt_again.read())
+# except Exception as e:
+#   print(str(e))
+txt = open(filename, "")
