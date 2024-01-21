f29a8762ceaf0a40b27c8b2537651bf5848c7e54
zhoujingfighting <254644528@qq.com>
2024-01-21 17:42:21 +0800

    Update

--- read.py
+++ read.py
@@ -1,5 +1,10 @@
 from sys import argv
-
+# close – Closes the file. Like File->Save.. in your editor.
+# • read – Reads the contents of the file. You can assign the result to a variable.
+# • readline – Reads just one line of a text file.
+# • truncate – Empties the file. Watch out if you care about the file.
+# • write('stuff') – Writes ”stuff” to the file.
+# • seek(0) – Move the read/write location to the beginning of the file.
 script, filename = argv
 
 # txt = open(filename)
